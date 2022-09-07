0fba5647d8eb558f427012ce54cab4564abdf37b
Arinpe <dehtolah@gmail.com>
2022-09-08 00:48:40 +0100

    remove

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ActionController::Base
   before_action :authorize_request
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :current_cart
 
