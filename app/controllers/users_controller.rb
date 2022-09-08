ebc03175c205c1638e6218a58b4ec234d15c63f2
Arinpe <dehtolah@gmail.com>
2022-09-08 01:08:22 +0100

    rename json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        token = JsonWebToken.encode(user_id: @user.id)
+        token = Jsonwebtoken.encode(user_id: @user.id)
         time = Time.now + 24.hours.to_i
         render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                        first_name: @user.first_name, last_name: @user.last_name, email: @user.email, health_care_address: @user.health_care_address, mobile_number: @user.mobile_number }, status: :created
